a11250e5de92a0d7238559453f6df110ff2fe10e
farhanmunada <farchanang@gmail.com>
2025-01-11 19:42:01 +0700

    fix edit feature

--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -1,41 +1,22 @@
-import { useRouter } from "next/router";
 import EditForm from "@/components/edit-form";
 import { getImagesById } from "@/lib/data";
 import { notFound } from "next/navigation";
-import { useEffect, useState } from "react";
 
-interface ImageData {
+interface Params {
   id: string;
-  title: string;
-  image: string;
-  createdAt: Date;
-  updatedAt: Date;
 }
 
-const EditPage = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  const [data, setData] = useState<ImageData | null>(null);
 
-  useEffect(() => {
-    if (id) {
-      getImagesById(id as string).then((result) => {
-        if (!result) {
-          notFound();
-        } else {
-          setData(result[0]);
-        }
-      });
-    }
-  }, [id]);
+const EditPage = async ({ params }: { params: Params }) => {
+  const data = await getImagesById(params.id);
 
-  if (!data) return null;
+  if (!data) return notFound();
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-slate-100">
       <div className="bg-white rounded-sm shadow p-8">
         <h1 className="text-2xl font-bold mb-5">Update Image</h1>
-        <EditForm data={data} />
+        <EditForm data={data[0]} />
       </div>
     </div>
   );
